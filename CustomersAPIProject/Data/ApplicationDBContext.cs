eecca8e51fd27e5b7bfca84add2033a159d7305f
Supraja Ramaraju <test.email@example.com>
2024-09-19 15:36:08 +0100

    Added migrations and successfully updated table records in the DB

--- CustomersAPIProject/Data/ApplicationDBContext.cs
+++ CustomersAPIProject/Data/ApplicationDBContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CustomersAPIProject.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CustomersAPIProject.Data
 {
@@ -7,6 +8,63 @@ namespace CustomersAPIProject.Data
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
         {
                 
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().HasData(
+                new Customer
+                {
+                    CustomerID = 1,
+                    Name = "Mike Smith",
+                    Address = "1 Wall Street",
+                    Email = "msmith@gmail.com",
+                    Phone = "0800 750 2219"
+                },
+                new Customer
+                {
+                    CustomerID = 2,
+                    Name = "John Hill",
+                    Address = "10 Swiss Cottage Street",
+                    Email = "jsmith@gmail.com",
+                    Phone = "0800 750 2219"
+                },
+                new Customer
+                {
+                    CustomerID = 3,
+                    Name = "Tom Davis",
+                    Address = "15 Logan Road",
+                    Email = "tdavis@gmail.com",
+                    Phone = "0800 750 2219"
+                },
+                new Customer
+                {
+                    CustomerID = 4,
+                    Name = "Will Brown",
+                    Address = "12 Forest Street",
+                    Email = "willb@yahoo.com",
+                    Phone = "0800 750 2219"
+                },
+                new Customer
+                {
+                    CustomerID = 5,
+                    Name = "Jess Smith",
+                    Address = "20 Fenchurch Road",
+                    Email = "jsmith@yahoo.com",
+                    Phone = "0800 750 2219"
+                },
+                new Customer
+                {
+                    CustomerID = 6,
+                    Name = "Kate Mills",
+                    Address = "25 Greenhill Lane",
+                    Email = "jnmiills@gmail.com",
+                    Phone = "0800 750 2219"
+                }
+    
+            );
         }
     }
 }
---
+++ CustomersAPIProject/Migrations/20240919142341_SeedCustomersTable.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CustomersAPIProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCustomersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerID", "Address", "Email", "Name", "Phone" },
+                values: new object[,]
+                {
+                    { 1, "1 Wall Street", "msmith@gmail.com", "Mike Smith", "0800 750 2219" },
+                    { 2, "10 Swiss Cottage Street", "jsmith@gmail.com", "John Hill", "0800 750 2219" },
+                    { 3, "15 Logan Road", "tdavis@gmail.com", "Tom Davis", "0800 750 2219" },
+                    { 4, "12 Forest Street", "willb@yahoo.com", "Will Brown", "0800 750 2219" },
+                    { 5, "20 Fenchurch Road", "jsmith@yahoo.com", "Jess Smith", "0800 750 2219" },
+                    { 6, "25 Greenhill Lane", "jnmiills@gmail.com", "Kate Mills", "0800 750 2219" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 6);
+        }
+    }
+}
