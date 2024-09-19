7575ee02d4364aa25d1c6054b567fbdabb574e9e
Supraja Ramaraju <test.email@example.com>
2024-09-19 11:37:13 +0100

    Add project files.

---
+++ CustomersAPIProject/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomersAPIProject.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+    }
+}
