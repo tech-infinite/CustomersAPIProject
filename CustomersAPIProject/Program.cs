8b06cf0fcd78137cc43e4423fbb0cb1964169bca
Supraja Ramaraju <test.email@example.com>
2024-09-19 12:11:06 +0100

    Configured connection strings in the dependency injection

--- CustomersAPIProject/Program.cs
+++ CustomersAPIProject/Program.cs
@@ -1,7 +1,11 @@
+using CustomersAPIProject.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+builder.Services.AddDbContext<ApplicationDBContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 var app = builder.Build();
